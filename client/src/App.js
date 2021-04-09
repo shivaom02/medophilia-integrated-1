79fb49dc342bcae1a408b6e1a53bfea63594ed22
DHRITIRAJ <dhritirajborah2020@gmail.com>
2021-04-09 12:57:25 +0530

    scanner added

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Page1 from './components/PharmacyPanel/page1/page1';
 import Page2 from './components/PharmacyPanel/page2/page2';
 
 import Index from './components/PharmacyPanel/Pharma/index';
-
+import Qrsanner from './components/qrScanner/qrsanner';
 // import Pharma from './components/PharmacyPanel/Pharma/index';
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
@@ -21,6 +21,7 @@ function App() {
                     <Route path='/page2' component={Page2}/>
 
                     <Route path='/index' component={Index}/>
+                    <Route path='/scanner' component={Qrsanner}/>
                </Switch>
             </div>
         </Router>
